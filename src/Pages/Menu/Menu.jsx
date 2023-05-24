e97b3bdf99d37c344e1c3245d440e036ad8e775a
emonh0que <you@example.com>
2023-05-24 17:00:27 +0600

    added paralax

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,4 +1,8 @@
 import {Helmet } from 'react-helmet-async';
+import Cover from '../Home/Home/Shared/Cover/Cover';
+
+import menuImg from '../../assets/menu/banner3.jpg';
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +10,12 @@ const Menu = () => {
             <Helmet>
                 <title>Menu | Food Client</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover title="Our Menu" img={menuImg}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover title="Our Menu" img={menuImg}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover title="Our Menu" img={menuImg}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
