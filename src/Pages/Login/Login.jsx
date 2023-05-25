f0a2d6e074efc62fc37463317e23cad9421a0a90
emonh0que <you@example.com>
2023-05-26 00:07:26 +0600

    added react hook form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,17 @@
 
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
 
 
 const Login = () => {
+
+
     const [disabled, setDisabled] = useState(true);
+
+    const {signIn} = useContext(AuthContext);
 
     useEffect(() => { 
         loadCaptchaEnginge(6)
@@ -16,6 +22,14 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     const captchaRef = useRef(null);
@@ -23,6 +37,9 @@ const Login = () => {
         const user_captcha_value = captchaRef.current.value;
         if(validateCaptcha(user_captcha_value)){
             setDisabled(false);
+        }
+        else{
+            setDisabled(true)
         }
         
     }
@@ -86,6 +103,7 @@ const Login = () => {
               <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
             </div>
           </form>
+          <p>New Here? <Link to="/signup">Create an account</Link></p>
         </div>
       </div>
     </div>
